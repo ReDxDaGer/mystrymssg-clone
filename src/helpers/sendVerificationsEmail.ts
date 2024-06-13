11fa7a5cc4cdcc8ce09da3e16380f230bde0e2ee
ReDxDaGer <yashmehrotra45@gmail.com>
2024-06-13 22:39:01 +0530

    Updated the helpers (sending Verification Email)

--- src/helpers/sendVerificationsEmail.ts
+++ src/helpers/sendVerificationsEmail.ts
@@ -1,1 +1,24 @@
 import {resend} from '@/lib/resend';
+import VerificationEmail from '../../emails/VerificationEmail';
+import { ApiResponse } from '@/types/ApiResponse';
+
+
+export async function sendVerificationsEmail(
+    email: string,
+    username: string,
+    password: string,
+    verifyCode: string): Promise<ApiResponse>{
+        try{
+            await resend.emails.send({
+                from: 'onboarding@resend.dev',
+                to: email,
+                subject: 'Mystry Verification Code',
+                react: VerificationEmail({username , otp: verifyCode}),
+              });
+
+            return {success: true, message: "Email sent successfully"};
+        }catch(emailError){
+            console.error("Error Sending verification email !!",emailError);
+            return {success: false, message: "Error Sending verification email"};
+        }     
+};
